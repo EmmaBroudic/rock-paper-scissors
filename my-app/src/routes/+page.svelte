18993a45597b7ac7c781ad50b7bed8e7b3afb76d
unknown <emma.broudic35@gmail.com>
2024-01-15 11:16:45 +0100

    counter added

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Button from './Button.svelte'
+</script>
+
+<body>
+
+    <h1>Rock, paper, scissors</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+    <div id="button-bloc">
+
+        <div class="button">
+            <Button />
+        </div>
+
+        <div class="button">
+            <button>Rock</button>
+        </div>
+
+        <div class="button">
+            <button>Paper</button>
+        </div>
+
+        <div class="button">
+            <button>Scissors</button>
+        </div>
+    </div>
+
+</body>
